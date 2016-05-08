2ae4397fbe0ecf6614a74b10ddb0e33139a8a20f
François Fouquemberg <francois.fouquemberg@gmail.com>
2016-05-08 21:17:44 +0200

    new BDD

--- circuit2000OLD.sql
+++ circuit2000OLD.sql
@@ -2,10 +2,10 @@
 -- version 4.2.10
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: May 08, 2016 at 06:37 PM
--- Server version: 5.5.38
--- PHP Version: 5.6.2
+-- Client :  localhost
+-- Généré le :  Mar 05 Avril 2016 à 23:17
+-- Version du serveur :  5.5.38
+-- Version de PHP :  5.6.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,13 +17,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `circuit2000`
+-- Base de données :  `circuit2000`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `cours`
+-- Structure de la table `cours`
 --
 
 CREATE TABLE `cours` (
@@ -34,26 +34,22 @@ CREATE TABLE `cours` (
   `date` datetime DEFAULT NULL,
   `description` varchar(60) DEFAULT NULL,
   `Commentaire` varchar(40) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `cours`
+-- Contenu de la table `cours`
 --
 
 INSERT INTO `cours` (`id`, `idMoniteur`, `idEleve`, `temps_pause`, `date`, `description`, `Commentaire`) VALUES
-(1, 1, 1, NULL, '2016-03-30 08:00:00', 'Mon premier cours', 'Voici les commentaires du premier cours.'),
-(2, 1, 1, NULL, '2016-03-31 15:00:00', 'Mon Deuxième cours', 'Commentaires du 2eme cours'),
-(3, 1, 1, NULL, '2016-03-02 16:00:00', 'Un cours en semaine 9', 'Commentaire d''un cours en semaine 9'),
-(4, 1, 1, NULL, '2016-04-06 11:00:00', 'Cours avec le moniteur 3 et l''élève 4', 'un Com'' avec le moniteur 3 et l''élève 4'),
-(5, 1, 1, NULL, '2016-05-03 08:00:00', 'Un cours de test', NULL),
-(7, 1, 1, NULL, '2016-05-04 08:00:00', 'Un 2ème cours de test', NULL),
-(8, 1, 1, NULL, '2016-05-01 10:00:00', 'Un troisième test', NULL),
-(9, 1, 1, NULL, '2016-05-05 11:00:00', 'Un autre test', NULL);
+(1, 1, 4, NULL, '2016-03-30 08:00:00', 'Mon premier cours', 'Voici les commentaires du premier cours.'),
+(2, 1, 4, NULL, '2016-03-31 15:00:00', 'Mon Deuxième cours', 'Commentaires du 2eme cours'),
+(3, 1, 4, NULL, '2016-03-02 16:00:00', 'Un cours en semaine 9', 'Commentaire d''un cours en semaine 9'),
+(4, 3, 4, NULL, '2016-04-06 11:00:00', 'Cours avec le moniteur 3 et l''élève 4', 'un Com'' avec le moniteur 3 et l''élève 4');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `eleve`
+-- Structure de la table `eleve`
 --
 
 CREATE TABLE `eleve` (
@@ -65,7 +61,7 @@ CREATE TABLE `eleve` (
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `eleve`
+-- Contenu de la table `eleve`
 --
 
 INSERT INTO `eleve` (`id`, `idEleve`, `dateInscription`, `dateCode`, `datePermis`) VALUES
@@ -74,7 +70,7 @@ INSERT INTO `eleve` (`id`, `idEleve`, `dateInscription`, `dateCode`, `datePermis
 -- --------------------------------------------------------
 
 --
--- Table structure for table `fonction`
+-- Structure de la table `fonction`
 --
 
 CREATE TABLE `fonction` (
@@ -83,7 +79,7 @@ CREATE TABLE `fonction` (
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `fonction`
+-- Contenu de la table `fonction`
 --
 
 INSERT INTO `fonction` (`numero`, `nom`) VALUES
@@ -93,7 +89,7 @@ INSERT INTO `fonction` (`numero`, `nom`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `moniteur`
+-- Structure de la table `moniteur`
 --
 
 CREATE TABLE `moniteur` (
@@ -104,16 +100,16 @@ CREATE TABLE `moniteur` (
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `moniteur`
+-- Contenu de la table `moniteur`
 --
 
 INSERT INTO `moniteur` (`id`, `idMoniteur`, `dateEntree`, `immatVoiture`) VALUES
-(2, 1, '2016-03-29', '3445');
+(4, 1, '2016-03-29', '3445');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Structure de la table `user`
 --
 
 CREATE TABLE `user` (
@@ -132,18 +128,18 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `user`
+-- Contenu de la table `user`
 --
 
 INSERT INTO `user` (`id`, `nom`, `prenom`, `dateNaissance`, `mail`, `mdp`, `fonction`, `numeroADR`, `rueADR`, `villeADR`, `codePostal`, `telephone`) VALUES
-(1, 'nomAdmin', 'prenomAdmin', '24/06/1994', 'admin', 'admin', 1, '1', 'rue des fleurs', 'Paris', '95001', '0645716659'),
-(2, 'nomMoniteur', 'prenomMoniteur', '13/12/95', 'moniteur', 'moniteur', 2, '45', 'Jean Philippe', 'Lens', '62300', '0123456789'),
-(3, 'nomClient', 'prenomClient', '24/05/88', 'eleve', 'eleve', 0, '34', 'Alfred 2 ', 'Lille', '59000', '1234567891');
+(2, 'Eve', 'Flo', '24/06/1994', 'admin', 'admin', 1, '1', '1', '1', ' 62153', '0645716659'),
+(3, 'nomAdmin', 'prenomAdmin', '13/12/95', 'moniteur', 'moniteur', 2, '45', 'Jean Philippe', 'Lens', '62300', '0123456789'),
+(4, 'nomMoniteur', 'prenomMoniteur', '24/05/88', 'moniteur@moniteur.com', 'moniteur', 0, '34', 'Alfred 2 ', 'Lille', '59000', '1234567891');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `vehicule`
+-- Structure de la table `vehicule`
 --
 
 CREATE TABLE `vehicule` (
@@ -156,76 +152,76 @@ CREATE TABLE `vehicule` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Indexes for dumped tables
+-- Index pour les tables exportées
 --
 
 --
--- Indexes for table `cours`
+-- Index pour la table `cours`
 --
 ALTER TABLE `cours`
  ADD PRIMARY KEY (`id`), ADD KEY `FK_Cours_id` (`idEleve`);
 
 --
--- Indexes for table `eleve`
+-- Index pour la table `eleve`
 --
 ALTER TABLE `eleve`
  ADD PRIMARY KEY (`idEleve`);
 
 --
--- Indexes for table `fonction`
+-- Index pour la table `fonction`
 --
 ALTER TABLE `fonction`
  ADD PRIMARY KEY (`numero`);
 
 --
--- Indexes for table `moniteur`
+-- Index pour la table `moniteur`
 --
 ALTER TABLE `moniteur`
  ADD PRIMARY KEY (`idMoniteur`);
 
 --
--- Indexes for table `user`
+-- Index pour la table `user`
 --
 ALTER TABLE `user`
  ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `vehicule`
+-- Index pour la table `vehicule`
 --
 ALTER TABLE `vehicule`
  ADD PRIMARY KEY (`immatriculation`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT pour les tables exportées
 --
 
 --
--- AUTO_INCREMENT for table `cours`
+-- AUTO_INCREMENT pour la table `cours`
 --
 ALTER TABLE `cours`
-MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
+MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 --
--- AUTO_INCREMENT for table `eleve`
+-- AUTO_INCREMENT pour la table `eleve`
 --
 ALTER TABLE `eleve`
 MODIFY `idEleve` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
--- AUTO_INCREMENT for table `fonction`
+-- AUTO_INCREMENT pour la table `fonction`
 --
 ALTER TABLE `fonction`
 MODIFY `numero` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
--- AUTO_INCREMENT for table `moniteur`
+-- AUTO_INCREMENT pour la table `moniteur`
 --
 ALTER TABLE `moniteur`
 MODIFY `idMoniteur` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
--- AUTO_INCREMENT for table `user`
+-- AUTO_INCREMENT pour la table `user`
 --
 ALTER TABLE `user`
 MODIFY `id` int(6) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 --
--- AUTO_INCREMENT for table `vehicule`
+-- AUTO_INCREMENT pour la table `vehicule`
 --
 ALTER TABLE `vehicule`
 MODIFY `immatriculation` bigint(20) NOT NULL AUTO_INCREMENT;
